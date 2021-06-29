106283cabeb1f5b2797f7738b5b34a82d879ad25
phuc nguyen <phuc.nguyen97haui@gamil.com>
2021-06-29 22:48:14 +0700

    add maven

--- quan-ly-ban-hang/src/main/java/com/sapo/quanlybanhang/entity/ProductEntity.java
+++ quan-ly-ban-hang/src/main/java/com/sapo/quanlybanhang/entity/ProductEntity.java
@@ -1,8 +1,6 @@
 package com.sapo.quanlybanhang.entity;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Collection;
