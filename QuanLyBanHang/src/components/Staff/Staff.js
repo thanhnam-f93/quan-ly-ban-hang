896cab7c50e7d0fbcab61e69ee9b3eb594317483
leducbinh <ducbinhbld>
2021-07-01 10:44:02 +0700

    add font end

---
+++ QuanLyBanHang/src/components/Staff/Staff.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import {
+  CBadge,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CDataTable,
+  CRow
+} from '@coreui/react'
+import { DocsLink } from 'src/reusable'
+
+import usersData from '../.././views/users/UsersData'
+
+const getBadge = status => {
+  switch (status) {
+    case 'Active': return 'success'
+    case 'Inactive': return 'secondary'
+    case 'Pending': return 'warning'
+    case 'Banned': return 'danger'
+    default: return 'primary'
+  }
+}
+const fields = ['name','registered', 'role', 'status']
+
+const Staff = () => {
+  return (
+    <>
+      <CRow>
+        <CCol>
+          <CCard>
+            <CCardHeader>
+              Combined All Table
+            </CCardHeader>
+            <CCardBody>
+            <CDataTable
+              items={usersData}
+              fields={fields}
+              hover
+              striped
+              bordered
+              size="sm"
+              itemsPerPage={10}
+              pagination
+              scopedSlots = {{
+                'status':
+                  (item)=>(
+                    <td>
+                      <CBadge color={getBadge(item.status)}>
+                        {item.status}
+                      </CBadge>
+                    </td>
+                  )
+              }}
+            />
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>    
+    </>
+  )
+}
+
+export default Staff
