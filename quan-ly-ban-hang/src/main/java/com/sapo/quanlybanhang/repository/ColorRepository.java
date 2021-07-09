f26a0d5dc2ef0019306de40b2b4e0dcb377516bf
quannd <quanxomtruyen@gmail.com>
2021-07-09 14:21:25 +0700

    push

--- quan-ly-ban-hang/src/main/java/com/sapo/quanlybanhang/repository/ColorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sapo.quanlybanhang.repository;
-
-import com.sapo.quanlybanhang.entity.ColorEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ColorRepository extends JpaRepository<ColorEntity,Integer> {
-}
--- quan-ly-ban-hang/src/main/java/com/sapo/quanlybanhang/repository/SizeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sapo.quanlybanhang.repository;
-
-import com.sapo.quanlybanhang.entity.SizeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SizeRepository extends JpaRepository<SizeEntity,Integer> {
-}
